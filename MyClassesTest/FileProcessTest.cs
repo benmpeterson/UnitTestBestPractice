d08d2235665443707b53be6fadcd447d5d184d03
ben.micah.peterson <ben.micah.peterson@gmail.com>
2017-06-25 13:25:04 -0400

    Initial Commit

---
+++ MyClassesTest/FileProcessTest.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClasses;
+using System;
+
+namespace MyClassesTest
+{
+  [TestClass]
+  public class FileProcessTest
+  {
+    [TestMethod]
+    public void FileNameDoesExist() {
+      FileProcess fp = new FileProcess();
+      bool fromCall;
+
+      fromCall = fp.FileExists(@"C:\Windows\Regedit.exe");
+
+      Assert.IsTrue(fromCall);
+    }
+
+    [TestMethod]
+    public void FileNameDoesNotExist() {
+      FileProcess fp = new FileProcess();
+      bool fromCall;
+
+      fromCall = fp.FileExists(@"C:\NotExists.bad");
+
+      Assert.IsFalse(fromCall);
+    }
+
+    [TestMethod]
+    public void FileNameNullOrEmpty_ThrowsArgumentNullException() {
+      FileProcess fp = new FileProcess();
+
+      try {
+        fp.FileExists("");
+      }
+      catch (ArgumentNullException) {
+        // Test was a success
+        return;
+      }
+
+      // Fail the test
+      Assert.Fail("Call to FileExists() did NOT throw an ArgumentNullException.");
+    }
+
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void FileNameNullOrEmpty_ThrowsArgumentNullException_UsingAttribute() {
+      FileProcess fp = new FileProcess();
+      
+      fp.FileExists("");
+    }
+  }
+}
---
+++ MyClassesTest/FileProcessTestStubs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using MyClasses;
+using System.IO;
+
+namespace MyClassesTest
+{
+  [TestClass]
+  public class FileProcessTestStubs
+  {
+    [TestMethod]
+    public void FileNameDoesExist() {
+      Assert.Inconclusive();
+    }
+
+    [TestMethod]
+    public void FileNameDoesNotExist() {
+      Assert.Inconclusive();
+    }
+
+    [TestMethod]
+    public void FileNameNullOrEmpty_ThrowsArgumentNullException() {
+      Assert.Inconclusive();
+    }
+  }
+}
