e48d61450d6e092ce3af8ccbae21f0bdeab6ec43
ben.micah.peterson <ben.micah.peterson@gmail.com>
2017-06-25 14:14:58 -0400

    Refactored Tests To Best Practices

--- MyClassesTest/FileProcessTest.cs
+++ MyClassesTest/FileProcessTest.cs
@@ -1,58 +1,85 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MyClasses;
 using System;
+using System.Configuration;
+using System.IO;
 
 namespace MyClassesTest
 {
-  [TestClass]
-  public class FileProcessTest
-  {
+    [TestClass]
+    public class FileProcessTest
+    {
         //Use Consts in Unit Testing when possible
         private const string BAD_FILE_NAME = @"C:\NotExists.bad";
+        private string _GoodFileName;
 
-    [TestMethod]
-    public void FileNameDoesExist() {
-      FileProcess fp = new FileProcess();
-      bool fromCall;
+        public TestContext TestContext { get; set; }
 
-      fromCall = fp.FileExists(@"C:\Windows\Regedit.exe");
+        [TestMethod]
+        public void FileNameDoesExist()
+        {
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+            
+            //Creates a temp file using app.config then deletes it automatically
+            SetGoodFileName();
+            TestContext.WriteLine("Creating the file: " + _GoodFileName);
+            File.AppendAllText(_GoodFileName, "Some Text");
+            TestContext.WriteLine("Testing the file: " + _GoodFileName);
+            fromCall = fp.FileExists(_GoodFileName);
+            File.Delete(_GoodFileName);
+            Assert.IsTrue(fromCall);
+        }
 
-      Assert.IsTrue(fromCall);
+        [TestMethod]
+        public void FileNameDoesNotExist()
+        {
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            fromCall = fp.FileExists(BAD_FILE_NAME);
+
+            Assert.IsFalse(fromCall);
+        }
+
+        public void SetGoodFileName()
+        {
+            _GoodFileName = ConfigurationManager.AppSettings["GoodFileName"];
+            if (_GoodFileName.Contains("[AppPath]"))
+            {
+                _GoodFileName = _GoodFileName.Replace("[AppPath]",
+                Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+            }
+        }
+
+
+        [TestMethod]
+        public void FileNameNullOrEmpty_ThrowsArgumentNullException()
+        {
+            FileProcess fp = new FileProcess();
+
+            try
+            {
+                fp.FileExists("");
+            }
+            catch (ArgumentNullException)
+            {
+                // Test was a success
+                return;
+            }
+
+            // Fail the test
+            Assert.Fail("Call to FileExists() did NOT throw an ArgumentNullException.");
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FileNameNullOrEmpty_ThrowsArgumentNullException_UsingAttribute()
+        {
+            FileProcess fp = new FileProcess();
+
+            fp.FileExists("");
+        }
     }
-
-    [TestMethod]
-    public void FileNameDoesNotExist() {
-      FileProcess fp = new FileProcess();
-      bool fromCall;
-
-      fromCall = fp.FileExists(BAD_FILE_NAME);
-
-      Assert.IsFalse(fromCall);
-    }
-
-    [TestMethod]
-    public void FileNameNullOrEmpty_ThrowsArgumentNullException() {
-      FileProcess fp = new FileProcess();
-
-      try {
-        fp.FileExists("");
-      }
-      catch (ArgumentNullException) {
-        // Test was a success
-        return;
-      }
-
-      // Fail the test
-      Assert.Fail("Call to FileExists() did NOT throw an ArgumentNullException.");
-    }
-
-
-    [TestMethod]
-    [ExpectedException(typeof(ArgumentNullException))]
-    public void FileNameNullOrEmpty_ThrowsArgumentNullException_UsingAttribute() {
-      FileProcess fp = new FileProcess();
-      
-      fp.FileExists("");
-    }
-  }
 }
